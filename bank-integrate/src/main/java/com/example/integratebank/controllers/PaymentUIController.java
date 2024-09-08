a13ff3b341322dc562f450ba5c0a9e0e9e51c0bb
anhnbt <anhnbt@sparkminds.net>
2024-09-08 22:25:28 +0700

    Init UI <3

---
+++ bank-integrate/src/main/java/com/example/integratebank/controllers/PaymentUIController.java
@@ -0,0 +1,57 @@
+package com.example.integratebank.controllers;
+
+import com.example.integratebank.dto.CardInfoRequestDTO;
+import com.example.integratebank.dto.PaymentDTO;
+import com.example.integratebank.dto.SCBConfirmDTO;
+import com.example.integratebank.enumeration.PaymentProvider;
+import com.example.integratebank.payment.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+public class PaymentUIController {
+
+    private final PaymentService paymentService;
+
+    @GetMapping("/pay")
+    public String pay(Model model) {
+        List<String> providers = Arrays.asList(PaymentProvider.SCB.name(), PaymentProvider.KBANK.name());
+        PaymentDTO paymentDTO = new PaymentDTO();
+        model.addAttribute("paymentDto", paymentDTO);
+        model.addAttribute("providers", providers);
+        return "pay";
+    }
+
+    @PostMapping("/do-pay")
+    public String doPay(Model model, @ModelAttribute("paymentDto") PaymentDTO paymentDto) {
+        Map<String, String> data = paymentService.createPayment(paymentDto);
+        CardInfoRequestDTO cardInfoRequestDTO  = new CardInfoRequestDTO();
+        model.addAttribute("cardInfo", cardInfoRequestDTO);
+        model.addAttribute("data", data);
+        model.addAttribute("bankProvider", paymentDto.getProvider().name());
+        if (paymentDto.getProvider().equals(PaymentProvider.KBANK)) {
+            model.addAttribute("s", PaymentProvider.KBANK.name());
+        }
+        return "confirmpay";
+    }
+
+    @PostMapping("/confirm-pay")
+    public String confirmPay(Model model, @ModelAttribute("cardInfo") CardInfoRequestDTO dto) {
+        SCBConfirmDTO scbConfirmDTO = paymentService.submitPayment(dto);
+        if (scbConfirmDTO.isSuccess()) {
+            model.addAttribute("status", "success");
+        } else {
+            model.addAttribute("status", "failed");
+        }
+        return "index";
+    }
+}
